3b1a5d4c7fe400f754aca56b25184cf5abcccab2
nacho <nachope1201>
2020-11-24 16:31:15 -0300

    hash

--- hash.c
+++ hash.c
@@ -1,9 +1,12 @@
 #include <stdlib.h>
-#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
 #include "hash.h"
 #include "lista.h"
-//Hola.
+
 // ********** Definiciones **********
+
+#define TAM_DEFAULT 101
 
 typedef struct nodo{
 	char* clave;
@@ -12,14 +15,14 @@ typedef struct nodo{
 
 struct hash{
 	lista_t** listas;
-	size_t tam:
+	size_t tam;
 	size_t cant;
 	hash_destruir_dato_t destruir_dato;
 };
 
 struct hash_iter{
 	lista_iter_t* lista_iter;
-	size_t indice_actual;
+	size_t actual;
 	const hash_t* hash;
 };
 
@@ -27,8 +30,25 @@ struct hash_iter{
 
 // Función de hashing, la versión mas simple es generar la clave segun el string, pero también es estupidamente ineficiente.
 // La idea es usar una de las 2 primeras listadas en:: www.cse.yorku.ca/~oz/hash.html
-size_t f_hash(char* str){
- 	
+size_t f_hash(unsigned char *str){
+	size_t hash = 5381;
+    int c;
+    while (c = *str++){
+        hash = hash * 33 + c;
+	}
+	return hash;
+}
+
+static bool ubicar_listas(lista_t* lista){
+	bool no_error = true;
+	for(size_t i = 0; i < TAM_DEFAULT;i++){
+		lista_t* lista_a_insertar = lista_crear();
+		if(!lista_a_insertar){
+			no_error = false;
+		}
+		lista_insertar_ultimo(lista,lista_a_insertar);
+	}
+	return no_error;
 }
 
 //Busca un nodo dentro de la tabla hash.
@@ -37,7 +57,23 @@ nodo_t* buscar_nodo(const hash_t* hash; char* clave);
 
 // ********** Primitivas **********
 
-hash_t *hash_crear(hash_destruir_dato_t destruir_dato);
+hash_t *hash_crear(hash_destruir_dato_t destruir_dato){
+	hash_t* hash = malloc(sizeof(hash_t));
+	if(!hash){
+		return NULL;
+	}
+	hash->destruir_dato = destruir_dato;
+	hash->tam = TAM_DEFAULT;
+	hash->cant = 0;
+	hash->listas = lista_crear();
+	if(!hash->listas){
+		return NULL;
+	}
+	if(!ubicar_listas(hash->listas)){
+		return NULL;
+	}
+	return hash;
+}
 
 /* Guarda un elemento en el hash, si la clave ya se encuentra en la
  * estructura, la reemplaza. De no poder guardarlo devuelve false.
@@ -58,7 +94,10 @@ void *hash_borrar(hash_t *hash, const char *clave);
  * devuelve NULL.
  * Pre: La estructura hash fue inicializada
  */
-void *hash_obtener(const hash_t *hash, const char *clave);
+void *hash_obtener(const hash_t *hash, const char *clave){
+
+}
+
 
 /* Determina si clave pertenece o no al hash.
  * Pre: La estructura hash fue inicializada
@@ -68,14 +107,25 @@ bool hash_pertenece(const hash_t *hash, const char *clave);
 /* Devuelve la cantidad de elementos del hash.
  * Pre: La estructura hash fue inicializada
  */
-size_t hash_cantidad(const hash_t *hash);
+size_t hash_cantidad(const hash_t *hash){
+	return hash->cant;
+}
 
 /* Destruye la estructura liberando la memoria pedida y llamando a la función
  * destruir para cada par (clave, dato).
  * Pre: La estructura hash fue inicializada
  * Post: La estructura hash fue destruida
  */
-void hash_destruir(hash_t *hash);
+void hash_destruir(hash_t *hash){
+	size_t cont = 0;
+	while(!lista_esta_vacia(hash->listas)){
+		lista_destruir(hash->listas[cont], hash->destruir_dato);
+		cont++;
+	}
+	lista_destruir(hash->listas,NULL);
+	free(hash);
+}
+
 
 /* Iterador del hash */
 
@@ -89,7 +139,12 @@ bool hash_iter_avanzar(hash_iter_t *iter);
 const char *hash_iter_ver_actual(const hash_iter_t *iter);
 
 // Comprueba si terminó la iteración
-bool hash_iter_al_final(const hash_iter_t *iter);
+bool hash_iter_al_final(const hash_iter_t *iter){
+	//Si el actual es igual al tamaño, se recorrieron todas las posiciones.
+	return iter->actual == iter->hash->tam;
+}
 
 // Destruye iterador
-void hash_iter_destruir(hash_iter_t *iter);
+void hash_iter_destruir(hash_iter_t *iter){
+	free(iter);
+}
